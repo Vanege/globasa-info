cdc9df752cba8cffe8b45a60f59cf78afb84e813
Vanege <11367082+Vanege@users.noreply.github.com>
2025-01-05 18:09:15 +0100

    .

--- app/articles/20250105-cel-na-no-morcu-am-fale-8000-kadam-fe-moy-din.ts
+++ app/articles/20250105-cel-na-no-morcu-am-fale-8000-kadam-fe-moy-din.ts
@@ -13,7 +13,7 @@ To sen meta-analisi of multi estudi har 47 471 person fe totalya. Estudi meter r
 
 ![Max kadam mena max sehaya](https://www.thelancet.com/cms/10.1016/S2468-2667(21)00302-9/asset/63956204-c61a-4e4d-b787-2f606a12a84b/main.assets/gr3_lrg.jpg)
 
-Fe folo, mi rekomenda cele fe 8 000 kadam tas moyto. Ren abil na istala aplikaxey fe telifon cel na meter numer kadam. Eger na anda posamusa yu, am xidu na samawatu duore podikasti or musika. Eger yu bonxanse, yu abil na anda ton ban alote. Pia na samawatu telifon bante sen ibleya eger yusu kapi-oretul linku telifon.  
+Fe folo, mi rekomenda cele fe 8 000 kadam tas moyte. Ren abil na istala aplikaxey fe telifon cel na meter numer kadam. Eger na anda posamusa yu, am xidu na samawatu duore podikasti or musika. Eger yu bonxanse, yu abil na anda ton ban alote. Pia na samawatu telifon bante sen ibleya eger yusu kapi-oretul linku telifon.  
 
 Na kadam sen bon cel maxpul xey: to maxbongi fisikali ji sikoli sehaya fe totalya. Fe fato, minus eger ren jogay, no hay bon seba cel na no fale minim 8 000 kadam fe moy din. Eger hay daytermo, am intizar axam, eger hay barix, am tonporta sui-tahamul-abil kotu.
 `
