3bfe8048652f3e67e4c1f93ca1a03f32a6c5b317
Vanege <11367082+Vanege@users.noreply.github.com>
2025-01-05 18:04:32 +0100

    cel na no morcu

---
+++ app/articles/20250105-cel-na-no-morcu-am-fale-8000-kadam-fe-moy-din.ts
@@ -0,0 +1,20 @@
+import { Article } from "../types";
+
+export const article: Article = {
+  title: "Cel na no morcu, am fale 8 000 kadam fe moy din",
+  author: "Vanege",
+  imageUrl: "https://images.pexels.com/photos/631986/pexels-photo-631986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  imageDescription: "https://www.pexels.com/photo/person-stands-on-brown-pathway-631986/",
+  isPublished: true,
+  content: `
+Mi vole ki hay max Globasa-yen. Fe folo, eger yu sen Globasa-yen, am no morcu. Prehay efetopul nasiha cel na no morcu: am fale kufi kadam fe moy din. Minim 8000 to fe moy din. Ren jixi dento kos resulta de maxim day meta-analisi tem hin tema: [https://www.thelancet.com/journals/lanpub/article/PIIS2468-2667(21)00302-9/fulltext](https://www.thelancet.com/journals/lanpub/article/PIIS2468-2667(21)00302-9/fulltext)
+
+To sen meta-analisi of multi estudi har 47 471 person fe totalya. Estudi meter risko fe morcu fol numer kadam. Fol resulta, folki ren max kadam, risko lilcu. Risko velosi lilcu finfe 8 000 kadam. Eger ren laoje min kom 60 nyan, minimum risko sen fe daju 9 000. Eger ren laoje max kom 60 nyan, folki ren max kadam, risko dureli max lilcu.
+
+![Max kadam mena max sehaya](https://www.thelancet.com/cms/10.1016/S2468-2667(21)00302-9/asset/63956204-c61a-4e4d-b787-2f606a12a84b/main.assets/gr3_lrg.jpg)
+
+Fe folo, mi rekomenda cele fe 8 000 kadam tas moyto. Ren abil na istala aplikaxey fe telifon cel na meter numer kadam. Eger na anda posamusa yu, am xidu na samawatu duore podikasti or musika. Eger yu bonxanse, yu abil na anda ton ban alote. Pia na samawatu telifon bante sen ibleya eger yusu kapi-oretul linku telifon.  
+
+Na kadam sen bon cel maxpul xey: to maxbongi fisikali ji sikoli sehaya fe totalya. Fe fato, minus eger ren jogay, no hay bon seba cel na no fale minim 8 000 kadam fe moy din. Eger hay daytermo, am intizar axam, eger hay barix, am tonporta sui-tahamul-abil kotu.
+`
+};
