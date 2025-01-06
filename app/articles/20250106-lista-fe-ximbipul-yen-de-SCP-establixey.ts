17c6e950eefe86607ce3d822a84ae104d4a150af
Vanege <11367082+Vanege@users.noreply.github.com>
2025-01-06 21:24:40 +0100

    Lista fe ximbipul yen de SCP-establixey

---
+++ app/articles/20250106-lista-fe-ximbipul-yen-de-SCP-establixey.ts
@@ -0,0 +1,21 @@
+import { Article } from "../types";
+
+export const article: Article = {
+  title: "Lista fe ximbipul yen de SCP-establixey",
+  author: "Vanege",
+  imageUrl: "https://scp-wiki.wdfiles.com/local--files/scp-529/SCP-529-new.jpg",
+  imageDescription: "http://scp-int.wikidot.com/unofficial:qgb:scp-529",
+  isPublished: true,
+  content: `
+Kam yu suki na doxo tem ximbipul yen har inxone nenordinari abilya? Am doxo wiki de SCP-establixey. SCP-establixey sen fiksyonli organisatim hu da xidu na lista ji na tosifu moy xey ji yen har sifa hu da no uje bexomin syensi.
+
+SCP-wiki sen day bukado tonergoli projeto feki moyun person harizin na fatmin ximbipul yen. Alo person abil na proya (or antiya) SCP-yen hu person suki da. Moyun SCP-yen hare suli numer, denpul kom SCP-529 (hu da sen meli myaw-bage) ji SCP-005 (hu da sen yawxe hu da abil na buka maxori kuflu).
+
+[http://scp-int.wikidot.com/unofficial:qgb:scp-529](http://scp-int.wikidot.com/unofficial:qgb:scp-529)  
+[http://scp-int.wikidot.com/unofficial:qgb:scp-005](http://scp-int.wikidot.com/unofficial:qgb:scp-005)
+
+Bannumer yen abil na sen hataripul. Denseba, in wiki, hay informe tem anjen intreaksyon. Fe ban mara, yen sen godomo hataripul tas sosyaltim, fe den kaso, yen bebujo-musi ji anjeli beawgi-musi.
+
+Hay multi fobine video-yuxixey har SCP-yen (or ilhamu). In denpul yuxixey, SCP-yen sen hurugido ji te (or to) xikar yuxiyen. Yuxiyen musi na fale banto durki te xidu na no morcu kos SCP-yen.
+`
+};
