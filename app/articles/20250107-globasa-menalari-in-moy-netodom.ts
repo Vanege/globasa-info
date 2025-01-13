503efce2b23cccd4f3e8c27f742255ed71f917be
Vanege <11367082+Vanege@users.noreply.github.com>
2025-01-13 21:06:45 +0100

    globasa-menalari in moy-netodom

---
+++ app/articles/20250107-globasa-menalari-in-moy-netodom.ts
@@ -0,0 +1,24 @@
+import { Article } from "../types";
+
+export const article: Article = {
+  title: "Menalari fe Globasa in moy netodom",
+  author: "Vanege",
+  imageUrl: "",
+  imageDescription: "",
+  isPublished: true,
+  content: `
+In Globasa.info, denwatu hu ren per-levita lexi, Globasa-basatayti precu ton maxpul informe ji lexiasel. Dento sahay na aham Globasa-texto. Na hare samapul funsyon in moy netodom sen ible. Cel dento, am kopi eskripu fe sayfa-peda ji am plasi to in kodi-dom (console) de yusu neto-brawsetul (Chrome, Firefox, jmt). Hay video fe infer cel na onexa.
+
+Eskripu yongu maxim nerleli versyon de Globasa-menalari. Fe asif, to no funsyon in bannumer netodom koski eskripu fale HTTP-peti cel exli netodom (cel na xorhare menalari) ji bannumer netodom (denpul kom Globasawiki) bloki denpul peti.
+
+Cel na hala hin problema, hay alo versyon de eskripu feki inhare de menalari uje sen in eskripu. Dento sengi eskripu dayday, mas nil exli HTTP-peti behaja. Denmaner, to funsyon pia in Globasawiki ji aloloka. 
+
+Unyum eskripu, am jandan kopi ji inplasi to in kodi-dom de brawsetul ji na presyon "Enter". Xafe dento, am idi per lexi hu da sonzay in Globasa:
+[https://gist.github.com/Vanege/c8518a96f280fc20c0b79242d18749ec](https://gist.github.com/Vanege/c8518a96f280fc20c0b79242d18749ec)
+
+Video: ku kemaner na yongu Globasa-menalari in moy netodom: https://www.youtube.com/watch?v=h9ykv0NBurk
+
+Duayum eskripu, hu da uje inhare menalari fe xoru fe 2025. Hin eskripu funsyon in max netodom, mas menalari no sen fe maxim nerleli versyon:
+[https://gist.github.com/Vanege/9960ec5d28b650391d2a20dc55046f97](https://gist.github.com/Vanege/9960ec5d28b650391d2a20dc55046f97)
+`
+};
